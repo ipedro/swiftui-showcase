34898aa79c205d8b6efe935074a8a0dec079a20e
Pedro Almeida <ip4dro@gmail.com>
2023-09-11 11:53:55 +0200

    remove selection

--- Sources/Showcase/Views/ShowcaseList/ShowcaseList.swift
+++ Sources/Showcase/Views/ShowcaseList/ShowcaseList.swift
@@ -1,30 +1,20 @@
 import SwiftUI
 
 public struct ShowcaseList<Icon: View>: View {
-    @State private var selection: ShowcaseItem.ID?
     let data: [ShowcaseItem]
     let icon: Icon
     
     public init(
         _ data: [ShowcaseItem],
-        selection: ShowcaseItem.ID? = nil,
         @ViewBuilder icon: () -> Icon
     ) {
-        self._selection = State(initialValue: selection)
         self.data = data
         self.icon = icon()
     }
     
     public var body: some View {
-        List(data, children: \.children, selection: $selection) { item in
-            NavigationLink(
-                isActive: .init(get: {
-                    item.id == selection
-                }, set: { newValue, transaction in
-                    if newValue {
-                        selection = item.id
-                    }
-                })) {
+        List(data, children: \.children) { item in
+            NavigationLink {
                 Showcase(item)
             } label: {
                 Label {
