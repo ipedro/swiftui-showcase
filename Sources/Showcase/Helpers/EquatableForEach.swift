71ffb9fe0628c0b109504b59e73b47dd22abc090
Pedro Almeida <ip4dro@gmail.com>
2025-11-13 01:43:35 +0100

    fix: restore file creation dates and improve fix script [skip ci]
    - Use awk instead of sed for more reliable date replacement
    - Add verification and verbose logging
    - Skip CI to avoid infinite loop

--- Sources/Showcase/Helpers/EquatableForEach.swift
+++ Sources/Showcase/Helpers/EquatableForEach.swift
@@ -1,6 +1,6 @@
 // EquatableForEach.swift
 // Copyright (c) 2025 Pedro Almeida
-// Created by Pedro Almeida on 11/13/25.
+// Created by Pedro Almeida on 09/01/24.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
